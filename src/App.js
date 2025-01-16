bd3b01b50ddaee4fbcbeb025ae395918de613837
yekaditya11 <yekaditya11@gmail.com>
2025-01-16 19:59:31 +0530

    update3

--- src/App.js
+++ src/App.js
@@ -12,18 +12,22 @@ const App = () => {
 
   return (
     <div>
-      <h1>A short Naration of Lorem Ipsum</h1>{" "}
-      {/* This matches the test expectation */}
+      {/* Change intro text to match test's expected value */}
+      <h4>A short Naration of Lorem Ipsum</h4>
+
       {/* Loading state */}
       {loading && <h4>Loading...</h4>}
+
       {/* Error state */}
       {error && <p>Error: {error}</p>}
+
       {/* Display posts */}
       {data && Array.isArray(data) && (
         <ul>
           {data.map((post) => (
             <li key={post.id}>
-              <strong>{post.title}</strong>
+              {/* Add class 'title' to the title element */}
+              <strong className="title">{post.title}</strong>
               <p>{post.body}</p>
             </li>
           ))}
